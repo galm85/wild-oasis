cd405bf0e704b249f8542a3a0bf444a081a9cf62
Gal <galm85@gmail.com>
2025-03-25 10:25:17 +0200

    add filter and sort to booking table

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,9 +2,24 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 
-export async function getBookings(){
-  const {data,error} = await supabase.from('bookings').select('* ,cabins(name),guests(fullName,email))');
+export async function getBookings({filter,sortBy}){
+
+  let query =  supabase
+      .from('bookings')
+      .select('* ,cabins(name),guests(fullName,email))');
+
+  // Filter
+  if(filter){
+    query = query[filter.method || 'eq'](filter.field,filter.value);
+  }
+
+  // SortBy
+  if(sortBy){
+    query = query.order(sortBy.field,{ascending:sortBy.direction === 'asc'});
+  }
   
+  const {data,error} = await query;
+
   if(error){
     console.error(error);
     throw new Error('Booking can not be loaded')
