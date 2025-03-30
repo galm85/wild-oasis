813ed1bb12912614f41bddf749d92c0f362b4ea0
Gal <galm85@gmail.com>
2025-03-30 14:59:23 +0300

    add stats to dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import { useCabins } from '../cabins/useCabins';
 import styled from "styled-components";
+import Spinner from '../../ui/Spinner';
+import Stats from "./Stats";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,26 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+
+function DashboardLayout() {
+
+  const {isLoading:isLoading1,bookings} = useRecentBookings();
+  const {isLoading:isLoading2,stays,confirmedStays,numDays} = useRecentStays();
+  const {isLoading:isLoading3,cabins} = useCabins();
+
+  if(isLoading1 || isLoading2 || isLoading3) return <Spinner/>
+
+  
+  return (
+   <StyledDashboardLayout>
+      <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCounts={cabins.length}/>
+      <div>statistics</div>
+      <div>activiti today</div>
+      <div>chart sty</div>
+   </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
+
