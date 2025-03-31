750824c7b7f72cda7c03c754d60f8da4acdd42c2
Gal <galm85@gmail.com>
2025-03-31 11:15:03 +0300

    remove login information

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState("gal@email.com");
-  const [password, setPassword] = useState("camila49");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const {login,isLoading} = useLogin();
 
   function handleSubmit(e) {
