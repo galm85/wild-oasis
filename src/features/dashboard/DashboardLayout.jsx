2bf55e4ea79a74e343e7e0e8703130687f784c96
Gal <galm85@gmail.com>
2025-03-31 10:31:38 +0300

    create today activities in dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from '../../ui/Spinner';
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -18,7 +19,7 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
 
   const {isLoading:isLoading1,bookings} = useRecentBookings();
-  const {isLoading:isLoading2,stays,confirmedStays,numDays} = useRecentStays();
+  const {isLoading:isLoading2,confirmedStays,numDays} = useRecentStays();
   const {isLoading:isLoading3,cabins} = useCabins();
 
   if(isLoading1 || isLoading2 || isLoading3) return <Spinner/>
@@ -27,7 +28,7 @@ function DashboardLayout() {
   return (
    <StyledDashboardLayout>
       <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCounts={cabins.length}/>
-      <div>activiti today</div>
+      <TodayActivity/>
       <DurationChart confirmedStays={confirmedStays}/>
       <SalesChart bookings={bookings} numDays={numDays}/>
    </StyledDashboardLayout>
